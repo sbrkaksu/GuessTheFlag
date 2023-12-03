9d650db1f183185db631a879bac07b96dc6c64fc
Berk Aksu <berkaksu79@gmail.com>
2023-12-03 11:26:07 +0300

    Initial Commit

---
+++ GuessingFlag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GuessingFlag
+//
+//  Created by Berk Aksu on 3.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessingFlag/GuessingFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingFlagApp.swift
+//  GuessingFlag
+//
+//  Created by Berk Aksu on 3.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessingFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
